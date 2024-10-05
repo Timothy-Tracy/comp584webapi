1e4d7d8199b36dde0e48db290e84f0e85951d7c0
Tracy <timothy.tracy.168@my.csun.edu>
2024-10-04 17:36:41 -0700

    more work

--- DataModel/MydbContext.cs
+++ DataModel/MydbContext.cs
@@ -4,13 +4,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataModel;
 
-public partial class DbContext : DbContext
+public partial class MydbContext : DbContext
 {
-    public DbContext()
+    public MydbContext()
     {
     }
 
-    public DbContext(DbContextOptions<DbContext> options)
+    public MydbContext(DbContextOptions<MydbContext> options)
         : base(options)
     {
     }
@@ -21,7 +21,7 @@ public partial class DbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=db;Trusted_Connection=True;MultipleActiveResultSets=true");
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=mydbGolden;Trusted_Connection=True;MultipleActiveResultSets=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
