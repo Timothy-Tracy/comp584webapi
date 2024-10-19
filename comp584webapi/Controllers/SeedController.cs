0204a8efd6ac7f17a079afaa4547493775a48e80
Tracy <timothy.tracy.168@my.csun.edu>
2024-10-18 17:45:18 -0700

    more progggg

--- comp584webapi/Controllers/SeedController.cs
+++ comp584webapi/Controllers/SeedController.cs
@@ -53,8 +53,49 @@ namespace comp584webapi.Controllers
 
             return new JsonResult(countriesByName.Count);
         }
+        [HttpPost("Cities")]
         public async Task<IActionResult> ImportCitiesAsync()
         {
+            Dictionary<string, Country> countries = await db.Countries//.AsNoTracking()
+            .ToDictionaryAsync(c => c.Name);
+
+            CsvConfiguration config = new(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+                HeaderValidated = null
+            };
+            int cityCount = 0;
+            using (StreamReader reader = new(_pathName))
+            using (CsvReader csv = new(reader, config))
+            {
+                IEnumerable<WorldCitiesCSV>? records = csv.GetRecords<WorldCitiesCSV>();
+                foreach (WorldCitiesCSV record in records)
+                {
+                    if (!countries.TryGetValue(record.country, out Country? value))
+                    {
+                        Console.WriteLine($"Not found country for {record.city}");
+                        return NotFound(record);
+                    }
+
+                    if (!record.population.HasValue || string.IsNullOrEmpty(record.city_ascii))
+                    {
+                        Console.WriteLine($"Skipping {record.city}");
+                        continue;
+                    }
+                    City city = new()
+                    {
+                        Name = record.city,
+                        Lat = record.lat,
+                        Lng = record.lng,
+                        Population = (int)record.population.Value,
+                        CountryId = value.Id
+                    };
+                    db.Cities.Add(city);
+                    cityCount++;
+                }
+                await db.SaveChangesAsync();
+            }
+            return new JsonResult(cityCount);
 
         }
     }
